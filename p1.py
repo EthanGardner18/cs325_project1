5db2b839607dc28a2bbb5c88a724683739f2bd30
EthanGardner18 <ethangrdnr@gmail.com>
2024-01-30 16:07:47 -0600

    initial setup

---
+++ p1.py
@@ -0,0 +1,15 @@
+# this project is finding 5 articles from a news website and scraping the words into a text file
+
+# link 1: https://www.cnn.com/2024/01/13/entertainment/lift-kevin-hart-netflix-algorithm/index.html
+# link 2: https://www.cnn.com/2024/01/10/entertainment/mean-girls-review/index.html
+# link 3: https://www.cnn.com/videos/entertainment/2024/01/13/america-ferrera-barbie-movie-intv-amanpour-vpx.cnn
+# link 4: https://www.cnn.com/videos/movies/2023/12/01/cnn-screen-time-wonka-timothee-chalamet.cnn
+# link 5: https://www.cnn.com/2023/03/23/entertainment/john-wick-4-review/index.html
+
+# My scraper is beautifulsoup
+
+from bs4 import BeautifulSoup
+
+
+
+def main():
