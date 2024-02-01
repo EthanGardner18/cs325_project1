2aa9ef3190936650f551bf3785cac98f5b92de59
EthanGardner18 <ethangrdnr@gmail.com>
2024-02-01 14:33:15 -0600

    update

--- p1.py
+++ p1.py
@@ -1,15 +1,29 @@
 # this project is finding 5 articles from a news website and scraping the words into a text file
 
-# link 1: https://www.cnn.com/2024/01/13/entertainment/lift-kevin-hart-netflix-algorithm/index.html
-# link 2: https://www.cnn.com/2024/01/10/entertainment/mean-girls-review/index.html
-# link 3: https://www.cnn.com/videos/entertainment/2024/01/13/america-ferrera-barbie-movie-intv-amanpour-vpx.cnn
-# link 4: https://www.cnn.com/videos/movies/2023/12/01/cnn-screen-time-wonka-timothee-chalamet.cnn
-# link 5: https://www.cnn.com/2023/03/23/entertainment/john-wick-4-review/index.html
+# link 1: https://www.foxnews.com/sports/hopeful-bettor-places-100000-wager-super-bowl-coin-toss-tails-never-fails
+# link 2: https://www.foxnews.com/sports/ex-nfl-star-jim-mcmahon-recalls-super-bowl-memories-wanted-forget-first-one
+# link 3: https://www.foxnews.com/sports/ex-longhorns-star-bijan-robinson-jokingly-agrees-horns-down-taunt-should-come-consequences
+# link 4: https://www.foxnews.com/sports/syracuse-legend-jim-boeheim-reveals-hardest-part-basketball-programs-conference-realignment
+# link 5: https://www.foxnews.com/sports/taylor-swift-frenzy-great-for-football-former-nfl-stars-say
 
 # My scraper is beautifulsoup
 
 from bs4 import BeautifulSoup
+import requests
+import json
 
+url1 = "https://www.foxnews.com/sports/hopeful-bettor-places-100000-wager-super-bowl-coin-toss-tails-never-fails"
+url2 = "https://www.foxnews.com/sports/ex-nfl-star-jim-mcmahon-recalls-super-bowl-memories-wanted-forget-first-one"
+url3 = "https://www.foxnews.com/sports/ex-longhorns-star-bijan-robinson-jokingly-agrees-horns-down-taunt-should-come-consequences"
+url4 = "https://www.foxnews.com/sports/syracuse-legend-jim-boeheim-reveals-hardest-part-basketball-programs-conference-realignment"
+url5 = "https://www.foxnews.com/sports/taylor-swift-frenzy-great-for-football-former-nfl-stars-say"
 
+result = requests.get(url2)
+doc = BeautifulSoup(result.text,"html.parser")
 
-def main():
+tag = doc.find("script", {"type": "application/ld+json"})
+
+if tag:
+    json_data = json.loads(tag.string)
+    article_body = json_data.get("articleBody", "")
+    print(article_body)
